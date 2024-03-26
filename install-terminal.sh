47d6e59b9a561609b33e3ba351cb63469b0814ac
Conner Olsen <81334211+ARealConner@users.noreply.github.com>
2024-03-26 00:57:35 -0400

    Refined terminal installation script
    The terminal installation script has been improved for better user experience. Changes include:
    - Enhanced comments for clarity
    - Added echo statements to inform users about the ongoing installations
    - Reordered some installations and added new ones like CodeWhisperer and 'nice to have' tools
    - Included a section for dotfiles installation
    - Adjusted lazyvim install process, including moving existing configurations to backup locations before cloning the new setup from LazyVim repository.

--- install-terminal.sh
+++ install-terminal.sh
@@ -1,15 +1,18 @@
-### Terminal
+## Terminal
 brew install kitty
 echo "Select the theme you want to install"
 kitten themes
 
-### zsh4humans
+## zsh4humans
+echo "Installing zsh4humans"
 if command -v curl >/dev/null 2>&1; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
 else
   sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
 fi
 
+## CodeWhisperer
+echo "Installing CodeWhisperer"
 brew install codewhisperer
 cw integrations install input-method
 echo "Open CodeWhisperer and follow the setup instructions"
@@ -18,34 +21,38 @@ open /Applications/CodeWhisperer.app
 echo "Press enter to continue..."
 read
 
-brew install neovim
-
-### Nice to have
+## Nice to have
+echo "Installing nice to have tools"
 brew install btop
 brew install eza
 brew install nnn
 brew install gh
 
-## for lazyvim
+## dotfiles
+echo "Installing dotfiles"
+curl https://raw.githubusercontent.com/ARealConner/dotfiles/main/.zshrc -o $HOME/.zshrc >/dev/null 2>&1
+
+source $HOME/.zshrc
+
+## lazyvim install:
+echo "Installing lazyvim"
+# for lazyvim
+brew install node
+brew install neovim
 brew install lazygit
 brew install ripgrep
 brew install fd
 
-curl https://raw.githubusercontent.com/ARealConner/dotfiles/main/.zshrc -o $HOME/.zshrc >/dev/null 2>&1
-
-## lazyvim install:
 # required
-mv ~/.config/nvim{,.bak}
+mv $HOME/.config/nvim{,.bak}
 
 # optional but recommended
-mv ~/.local/share/nvim{,.bak}
-mv ~/.local/state/nvim{,.bak}
-mv ~/.cache/nvim{,.bak}
+mv $HOME/.local/share/nvim{,.bak}
+mv $HOME/.local/state/nvim{,.bak}
+mv $HOME/.cache/nvim{,.bak}
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim
+git clone https://github.com/LazyVim/starter $HOME/.config/nvim
 
-rm -rf ~/.config/nvim/.git
+rm -rf $HOME/.config/nvim/.git
 
 nvim
-
-source $HOME/.zshrc
