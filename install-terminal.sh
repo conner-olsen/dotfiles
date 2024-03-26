d6f9d9ba97a28480a8c5a9f7514f873d039f294b
Conner Olsen <81334211+ARealConner@users.noreply.github.com>
2024-03-26 00:42:37 -0400

    Enhanced terminal installation script
    The terminal installation script has been improved for better user interaction and additional features. User prompts have been made more informative, and the order of some installations has been rearranged. Lazygit is now installed as part of lazyvim setup, which also includes ripgrep and fd installations. The script now handles existing nvim configurations by moving them to backup locations before cloning the new configuration from LazyVim/starter repository. Finally, it sources .zshrc at the end of execution.

--- install-terminal.sh
+++ install-terminal.sh
@@ -1,5 +1,6 @@
 ### Terminal
 brew install kitty
+echo "Select the theme you want to install"
 kitten themes
 
 ### zsh4humans
@@ -14,16 +15,37 @@ cw integrations install input-method
 echo "Open CodeWhisperer and follow the setup instructions"
 open /Applications/CodeWhisperer.app
 
-read -p "Press enter to continue..."
+echo "Press enter to continue..."
+read
 
 brew install neovim
 
 ### Nice to have
 brew install btop
-brew install lazygit
 brew install eza
 brew install nnn
 brew install gh
 
+## for lazyvim
+brew install lazygit
+brew install ripgrep
+brew install fd
+
 curl https://raw.githubusercontent.com/ARealConner/dotfiles/main/.zshrc -o $HOME/.zshrc >/dev/null 2>&1
 
+## lazyvim install:
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+rm -rf ~/.config/nvim/.git
+
+nvim
+
+source $HOME/.zshrc
