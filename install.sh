5eebe98bf435eeae8dbffe2565f0ae4bcf5e9c75
Conner <81334211+ARealConner@users.noreply.github.com>
2024-03-27 02:07:49 -0400

    Installation Script Addition - Added new installation script file - Included commands to fetch and execute various scripts from a remote repository - Scripts include: apps, desktop, terminal, and sketchybar installations

---
+++ install.sh
@@ -0,0 +1,4 @@
+curl -L https://raw.githubusercontent.com/arealconner/dotfiles/main/install-apps.sh | zsh
+curl -L https://raw.githubusercontent.com/arealconner/dotfiles/main/install-desktop.sh | zsh
+curl -L https://raw.githubusercontent.com/arealconner/dotfiles/main/install-terminal.sh | zsh
+curl -L https://raw.githubusercontent.com/arealconner/dotfiles/main/install-sketchybar.sh | zsh
