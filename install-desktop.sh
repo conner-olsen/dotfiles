c0b0e0815e073e7f83866b32d1f53189c1d2fb39
Conner <81334211+ARealConner@users.noreply.github.com>
2024-03-27 00:11:48 -0400

    Refactor installation script - Removed SketchyBar only installation option - Simplified installation types to "Install everything" and "Custom settings" - Removed SketchyBar specific installations and configurations - Streamlined configuration file handling for all installation types - Updated service start-up process for all installation types

--- install-desktop.sh
+++ install-desktop.sh
@@ -36,9 +36,8 @@ prompt_user() {
 clear_screen
 echo "Choose installation type:"
 echo "1. Install everything"
-echo "2. Install SketchyBar only"
-echo "3. Custom settings"
-read "installation_type?Enter your choice [1-3]: "
+echo "2. Custom settings"
+read "installation_type?Enter your choice [1-2]: "
 
 # Install Homebrew if not already installed
 clear_screen
@@ -57,7 +56,7 @@ if ! command -v brew &> /dev/null; then
 fi
 
 # Install essentials based on installation type
-if [[ $installation_type == 1 || $installation_type == 3 ]]; then
+if [[ $installation_type == 1 || $installation_type == 2 ]]; then
     clear_screen
     progress "Tapping Brew..."
     brew tap homebrew/cask-fonts >/dev/null 2>&1
@@ -74,14 +73,6 @@ if [[ $installation_type == 1 || $installation_type == 3 ]]; then
     success "Essential packages installed"
 fi
 
-# Install SketchyBar
-if [[ $installation_type == 1 || $installation_type == 2 ]]; then
-    clear_screen
-    progress "Installing SketchyBar..."
-    brew install sketchybar >/dev/null 2>&1
-    success "SketchyBar installed"
-fi
-
 # Install fonts
 clear_screen
 progress "Installing fonts..."
@@ -90,7 +81,7 @@ brew install font-meslo-for-powerlevel10k >/dev/null 2>&1
 success "Fonts installed"
 
 # Install Karabiner-Elements and custom bindings
-if [[ $installation_type == 1 ]] || ( [[ $installation_type == 3 ]] && prompt_user "Do you want to install Karabiner-Elements and custom bindings?" ); then
+if [[ $installation_type == 1 ]] || ( [[ $installation_type == 2 ]] && prompt_user "Do you want to install Karabiner-Elements and custom bindings?" ); then
     clear_screen
     if ! brew list karabiner-elements &> /dev/null; then
         progress "Installing Karabiner-Elements and custom bindings..."
@@ -107,7 +98,7 @@ if [[ $installation_type == 1 ]] || ( [[ $installation_type == 3 ]] && prompt_us
 fi
 
 # Install Raycast
-if [[ $installation_type == 1 ]] || ( [[ $installation_type == 3 ]] && prompt_user "Do you want to install Raycast?" ); then
+if [[ $installation_type == 1 ]] || ( [[ $installation_type == 2 ]] && prompt_user "Do you want to install Raycast?" ); then
     clear_screen
     if ! brew list raycast &> /dev/null; then
         progress "Installing Raycast..."
@@ -123,7 +114,7 @@ if [[ $installation_type == 1 ]] || ( [[ $installation_type == 3 ]] && prompt_us
 fi
 
 # Override macOS default settings
-if [[ $installation_type == 1 ]] || ( [[ $installation_type == 3 ]] && prompt_user "Do you want to override macOS default settings?" ); then
+if [[ $installation_type == 1 ]] || ( [[ $installation_type == 2 ]] && prompt_user "Do you want to override macOS default settings?" ); then
     clear_screen
     progress "Changing macOS defaults..."
     # Disable writing .DS_Store files on network and external volumes
@@ -180,135 +171,63 @@ if [[ $installation_type == 1 ]] || ( [[ $installation_type == 3 ]] && prompt_us
     success "macOS default settings updated"
 fi
 
-# Installing Fonts
+# Copying and checking out configuration files
 clear_screen
-progress "Installing additional fonts..."
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/latest/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf >/dev/null 2>&1
-mkdir -p $HOME/.config/sketchybar/plugins >/dev/null 2>&1
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/latest/icon_map.sh -o $HOME/.config/sketchybar/plugins/icon_map.sh >/dev/null 2>&1
-success "Additional fonts installed"
+progress "Planting Configuration Files..."
+rm -rf "$HOME/temp-dotfiles" >/dev/null 2>&1
+git clone https://github.com/ARealConner/dotfiles.git "$HOME/temp-dotfiles" >/dev/null 2>&1
 
-# Copying and checking out configuration files if not SketchyBar only installation
-if [[ $installation_type != 2 ]]; then
-    clear_screen
-    progress "Planting Configuration Files..."
-    rm -rf "$HOME/temp-dotfiles" >/dev/null 2>&1
-    git clone https://github.com/ARealConner/dotfiles.git "$HOME/temp-dotfiles" >/dev/null 2>&1
+mkdir -p "$HOME/.config/backups" >/dev/null 2>&1
+timestamp=$(date +%Y%m%d%H%M%S)
+mkdir -p "$HOME/.config/backups/$timestamp" >/dev/null 2>&1
 
-    mkdir -p "$HOME/.config/backups" >/dev/null 2>&1
-    timestamp=$(date +%Y%m%d%H%M%S)
-    mkdir -p "$HOME/.config/backups/$timestamp" >/dev/null 2>&1
+# Backup existing configuration directories if they exist
+for dir in yabai skhd borders; do
+    if [ -d "$HOME/.config/$dir" ]; then
+        mv "$HOME/.config/$dir" "$HOME/.config/backups/$timestamp/$dir" >/dev/null 2>&1
+    fi
+done
 
-    # Backup existing configuration directories if they exist
-    for dir in yabai skhd sketchybar borders; do
-        if [ -d "$HOME/.config/$dir" ]; then
-            mv "$HOME/.config/$dir" "$HOME/.config/backups/$timestamp/$dir" >/dev/null 2>&1
-        fi
-    done
+# Move configuration directories from the cloned repository if they exist
+for dir in yabai skhd borders; do
+    if [ -d "$HOME/temp-dotfiles/.config/$dir" ]; then
+        mv "$HOME/temp-dotfiles/.config/$dir" "$HOME/.config/$dir" >/dev/null 2>&1
+    fi
+done
 
-    # Move configuration directories from the cloned repository if they exist
-    for dir in yabai skhd borders; do
-        if [ -d "$HOME/temp-dotfiles/.config/$dir" ]; then
-            mv "$HOME/temp-dotfiles/.config/$dir" "$HOME/.config/$dir" >/dev/null 2>&1
-        fi
-    done
+rm -rf "$HOME/temp-dotfiles" >/dev/null 2>&1
+success "Configuration files planted"
 
-    rm -rf "$HOME/temp-dotfiles" >/dev/null 2>&1
-    success "Configuration files planted"
-fi
-
-## Start with FelixKratz setup as a base. 
-# Install SbarLua Plugin to use lua scripts in sketchybar
+# Start Services
 clear_screen
-progress "Installing SbarLua Plugin..."
-(git clone https://github.com/FelixKratz/SbarLua.git $HOME/tmp/SbarLua >/dev/null 2>&1 && cd $HOME/tmp/SbarLua/ >/dev/null 2>&1 && make install >/dev/null 2>&1 && rm -rf $HOME/tmp/SbarLua/ >/dev/null 2>&1)
-success "SbarLua Plugin installed"
+progress "Starting Services (grant permissions)..."
+# start yabai
+yabai --start-service >/dev/null 2>&1
+skhd --start-service >/dev/null 2>&1
+brew services start borders >/dev/null 2>&1
 
-# Clone FelixKratz dotfiles
-clear_screen
-progress "Cloning Config..."
-mkdir -p $HOME/tmp/dotfiles >/dev/null 2>&1
-git clone https://github.com/FelixKratz/dotfiles.git $HOME/tmp/dotfiles >/dev/null 2>&1
-mv $HOME/tmp/dotfiles/.config/sketchybar $HOME/.config/sketchybar >/dev/null 2>&1
-rm -rf $HOME/tmp/dotfiles >/dev/null 2>&1
-success "Config cloned"
+progress "Adding yabai to sudoers..."
+echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai >/dev/null 2>&1
 
-## Add custom configuration to sketchybar:
-clear_screen
-progress "Customizing SketchyBar configuration..."
-# Change apple icon to the arch linux icon
-sed -i '' 's/apple = "􀣺"/apple = "󰣇"/g' $HOME/.config/sketchybar/icons.lua
-# Charge bar settings
-if [ -f "$HOME/.config/sketchybar/bar.lua" ]; then
-  if ! grep -q "margin=10," "$HOME/.config/sketchybar/bar.lua"; then
-    sed -i '' '/sbar.bar({/,/})/{/})/i\
-    margin=10,\
-    shadow=on,\
-    border_color=colors.white,\
-    corner_radius=9,\
-    border_width=2,
-  }' "$HOME/.config/sketchybar/bar.lua"
-  fi
-fi
+csrutil status
+error "Important!: To gain full functionality, you must disable SIP."
+echo -n "Press Enter to open guide..."
+read
+open "https://github.com/koekeishiya/yabai/wiki/Disabling-System-Integrity-Protection"
 
-sed -i '' \
-    -e 's/\(black = \)0x[0-9a-fA-F]\{8\}/\10xff282828/' \
-    -e 's/\(white = \)0x[0-9a-fA-F]\{8\}/\10xffebdbb2/' \
-    -e 's/\(red = \)0x[0-9a-fA-F]\{8\}/\10xffcc241d/' \
-    -e 's/\(green = \)0x[0-9a-fA-F]\{8\}/\10xff98971a/' \
-    -e 's/\(blue = \)0x[0-9a-fA-F]\{8\}/\10xff458588/' \
-    -e 's/\(yellow = \)0x[0-9a-fA-F]\{8\}/\10xffd79921/' \
-    -e 's/\(orange = \)0x[0-9a-fA-F]\{8\}/\10xffd65d0e/' \
-    -e 's/\(magenta = \)0x[0-9a-fA-F]\{8\}/\10xffb16286/' \
-    -e 's/\(grey = \)0x[0-9a-fA-F]\{8\}/\10xff928374/' \
-    -e 's/\(transparent = \)0x[0-9a-fA-F]\{8\}/\10x00000000/' \
-    -e 's/\(bg = \)0xf02c2e34/\10xff282828/' \
-    -e 's/\(bg1 = \)0x[0-9a-fA-F]\{8\}/\10xff3c3836/' \
-    -e 's/\(bg2 = \)0x[0-9a-fA-F]\{8\}/\10xff504945/' \
-    $HOME/.config/sketchybar/colors.lua
-
-sed -i '' 's/os.date("%H:%M")/os.date("%I:%M")/' $HOME/.config/sketchybar/items/calendar.lua
-success "SketchyBar configuration customized"
-
-# Start Services if not SketchyBar only installation
-if [[ $installation_type != 2 ]]; then
-    clear_screen
-    progress "Starting Services (grant permissions)..."
-    # start yabai
-    yabai --start-service >/dev/null 2>&1
-    skhd --start-service >/dev/null 2>&1
-    brew services start borders >/dev/null 2>&1
-
-    progress "Adding yabai to sudoers..."
-    echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai >/dev/null 2>&1
-
-    csrutil status
-    error "Important!: To gain full functionality, you must disable SIP."
-    echo -n "Press Enter to open guide..."
-    read
-    open "https://github.com/koekeishiya/yabai/wiki/Disabling-System-Integrity-Protection"
-
-    echo "Notice: every time you update yabai, you must run the following command to update the sha256 in the sudoers file: suyabai"
-    success "Services started"
-fi
-
-# Start SketchyBar
-clear_screen
-progress "Starting SketchyBar..."
-brew services start sketchybar >/dev/null 2>&1
-success "SketchyBar started"
+echo "Notice: every time you update yabai, you must run the following command to update the sha256 in the sudoers file: suyabai"
+success "Services started"
 
 # Installation Summary
 clear_screen
 success "Installation Complete!"
 echo "Summary:"
-echo "- Installed essential packages: $([[ $installation_type == 1 || $installation_type == 3 ]] && echo "Yes" || echo "No")"
-echo "- Installed SketchyBar: $([[ $installation_type == 1 || $installation_type == 2 ]] && echo "Yes" || echo "No")"
-echo "- Installed Karabiner-Elements and custom bindings: $([[ $installation_type == 1 ]] && echo "Yes" || ([[ $installation_type == 3 ]] && prompt_user "Do you want to install Karabiner-Elements and custom bindings?" && echo "Yes" || echo "No"))"
-echo "- Installed Raycast: $([[ $installation_type == 1 ]] && echo "Yes" || ([[ $installation_type == 3 ]] && prompt_user "Do you want to install Raycast?" && echo "Yes" || echo "No"))"
-echo "- Overridden macOS default settings: $([[ $installation_type == 1 ]] && echo "Yes" || ([[ $installation_type == 3 ]] && prompt_user "Do you want to override macOS default settings?" && echo "Yes" || echo "No"))"
-echo "- Copied configuration files: $([[ $installation_type != 2 ]] && echo "Yes" || echo "No")"
-echo "- Started services: $([[ $installation_type != 2 ]] && echo "Yes" || echo "No")"
+echo "- Installed essential packages: $([[ $installation_type == 1 || $installation_type == 2 ]] && echo "Yes" || echo "No")"
+echo "- Installed Karabiner-Elements and custom bindings: $([[ $installation_type == 1 ]] && echo "Yes" || ([[ $installation_type == 2 ]] && prompt_user "Do you want to install Karabiner-Elements and custom bindings?" && echo "Yes" || echo "No"))"
+echo "- Installed Raycast: $([[ $installation_type == 1 ]] && echo "Yes" || ([[ $installation_type == 2 ]] && prompt_user "Do you want to install Raycast?" && echo "Yes" || echo "No"))"
+echo "- Overridden macOS default settings: $([[ $installation_type == 1 ]] && echo "Yes" || ([[ $installation_type == 2 ]] && prompt_user "Do you want to override macOS default settings?" && echo "Yes" || echo "No"))"
+echo "- Copied configuration files: Yes"
+echo "- Started services: Yes"
 echo ""
 echo "Notice: Your previous .config files are backed up in $HOME/.config/backups/$timestamp"
 
