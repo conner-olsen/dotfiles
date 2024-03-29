4a1f3f567226d35183de7f1d13260368c710a516
Conner <connix13@gmail.com>
2024-03-29 05:34:30 -0400

    Enhanced install.sh with Homebrew setup - Added check and installation for Homebrew if not present - Turned off Brew analytics post-installation - Ensured Homebrew is added to path if not already done

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
+# Install Homebrew if not already installed
+if ! command -v brew &> /dev/null; then
+    progress "Installing Brew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" >/dev/null 2>&1
+    brew analytics off >/dev/null 2>&1
+else
+    success "Brew is already installed"
+fi
+
+# Add homebrew to path if not already added
+if ! command -v brew &> /dev/null; then
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/conner/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
 curl -L https://raw.githubusercontent.com/arealconner/dotfiles/main/install-apps.sh | zsh
 curl -L https://raw.githubusercontent.com/arealconner/dotfiles/main/install-desktop.sh | zsh
 curl -L https://raw.githubusercontent.com/arealconner/dotfiles/main/install-terminal.sh | zsh
