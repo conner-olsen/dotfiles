6fd8df6e76b924b93a6bb2a5bed6a1d31af1854c
Conner Olsen <81334211+ARealConner@users.noreply.github.com>
2024-05-07 20:31:16 -0400

    Refactored sketchybar configuration
    - Reformatted the code for better readability
    - No changes in functionality or features

--- .config/sketchybar/bar.lua
+++ .config/sketchybar/bar.lua
@@ -2,14 +2,14 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-  topmost = "window",
-  height = 40,
-  color = colors.bar.bg,
-  padding_right = 2,
-  padding_left = 2,
-    margin=6,
-    shadow=on,
-    border_color=colors.white,
-    corner_radius=9,
-    border_width=2,
+	topmost = "window",
+	height = 40,
+	color = colors.bar.bg,
+	padding_right = 2,
+	padding_left = 2,
+	margin = 6,
+	shadow = on,
+	border_color = colors.white,
+	corner_radius = 9,
+	border_width = 2,
 })
