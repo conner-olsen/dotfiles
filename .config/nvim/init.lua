a3564cd5eadef72d49c50ccdf34cafe898d1de65
Conner Olsen <81334211+ARealConner@users.noreply.github.com>
2024-06-11 21:01:40 +0200

    reinstall nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,34 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-local lspconfig = require("lspconfig")
-
-lspconfig.cssls.setup({
-  settings = {
-    css = {
-      lint = {
-        unknownAtRules = "ignore",
-      },
-    },
-  },
-})
-
-lspconfig.cssls.setup({
-  settings = {
-    css = {
-      lint = {
-        unknownAtRules = "ignore",
-      },
-    },
-    scss = {
-      lint = {
-        unknownAtRules = "ignore",
-      },
-    },
-    less = {
-      lint = {
-        unknownAtRules = "ignore",
-      },
-    },
-  },
-})
+-- Set Python 3 host program
+vim.g.python3_host_prog = "/Users/conner/.pyenv/versions/neovim3/bin/python"
