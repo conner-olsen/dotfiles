e7b21a689af66223e182602146b5b369e13796b0
Conner Olsen <81334211+ARealConner@users.noreply.github.com>
2024-06-11 20:17:07 +0200

    Added LSP configurations for CSS, SCSS, and LESS
    - Added Language Server Protocol (LSP) configuration for CSS
    - Extended the LSP configuration to support SCSS and LESS
    - Set linting rule to ignore unknown at-rules in all three languages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,34 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local lspconfig = require("lspconfig")
+
+lspconfig.cssls.setup({
+  settings = {
+    css = {
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+  },
+})
+
+lspconfig.cssls.setup({
+  settings = {
+    css = {
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+    scss = {
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+    less = {
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+  },
+})
