bef341c0e4031fdead4f2e708202ef395c6baa75
Conner Olsen <81334211+ARealConner@users.noreply.github.com>
2024-06-12 21:20:36 +0200

    Enhanced no-neck-pain plugin configuration
    - Added width parameter to the setup
    - Introduced autocmds with enableOnVimEnter and enableOnTabEnter options
    - Enabled mappings in the configuration

--- .config/nvim/lua/plugins/extras/no-neck-pain.lua
+++ .config/nvim/lua/plugins/extras/no-neck-pain.lua
@@ -1,3 +1,15 @@
 return {
   "shortcuts/no-neck-pain.nvim",
+  config = function()
+    require("no-neck-pain").setup({
+      width = 150,
+      autocmds = {
+        enableOnVimEnter = true,
+        enableOnTabEnter = true,
+      },
+      mappings = {
+        enable = true,
+      },
+    })
+  end,
 }
