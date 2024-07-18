4a4360deb073fd727dd3cf35179592534a053f69
Conner <arealconner@gmail.com>
2024-07-19 01:14:39 +0200

    Update application installation script - Removed installation of specific browser versions and Adobe Acrobat Pro - Added Arc browser, tesseract for text recognition, and git lfs install command - Commented out some development tools installations (Webstorm, Node, Yarn) with a TODO to add alternatives - Updated Python setup to include pyenv-virtualenv and commented out specific version setup with a TODO to automate latest python setup - Removed dataspell from data science tools

--- install-apps.sh
+++ install-apps.sh
@@ -3,9 +3,7 @@
 brew install 1password
 
 ## Browsers
-brew tap homebrew/cask-versions
-brew install google-chrome-beta
-brew install microsoft-edge-beta
+brew install --cask arc
 
 ## Communication
 brew install discord
@@ -15,32 +13,32 @@ brew install slack
 brew install microsoft-excel
 brew install microsoft-word
 brew install microsoft-powerpoint
-brew install adobe-acrobat-pro
 
 ## Hardware
 brew install logi-options-plus
 
 ## Development
 # Generic IDEs
-brew install visual-studio-code
 brew install cursor
 brew install zed
 
 # Web
-brew install webstorm
-brew install node
-brew install yarn
-yarn config set --home enableTelemetry 0
+# TODO: Add bun and node
 
-brew install postgresql@16
+# TODO: Add postgre
 
 # Python
 brew install pipenv
 brew install pyenv
-pyenv install 3.12.2
-pyenv global 3.12.2
-brew install jupyterlab
-brew install pycharm
+brew install pyenv-virtualenv
+# TODO: Automate latest python setup
+# pyenv install 3.12.2
+# pyenv global 3.12.2
+# TODO: Check if needed:
+# brew install jupyterlab
+
+# Text recognition
+brew install tesseract
 
 # Rust
 brew install rustrover
@@ -48,10 +46,8 @@ brew install rustup-init
 
 # Git
 brew install git-lfs
+git lfs install
 brew install gitkraken
-
-# Datascience
-brew install dataspell
 
 ## Installations
 rustup-init
