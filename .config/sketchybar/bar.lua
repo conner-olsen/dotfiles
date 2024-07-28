5d1a62745d8d2ed88ae3508eb2312cc6a8aacabe
ARealConner <arealconner@gmail.com>
2024-07-28 15:23:28 -0400

    Updated bar properties in sketchybar config - Reduced height from 40 to 32 - Removed margin, shadow, border_color, corner_radius and border_width properties

--- .config/sketchybar/bar.lua
+++ .config/sketchybar/bar.lua
@@ -3,13 +3,8 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
 	topmost = "window",
-	height = 40,
+	height = 32,
 	color = colors.bar.bg,
 	padding_right = 2,
 	padding_left = 2,
-	margin = 6,
-	shadow = on,
-	border_color = colors.white,
-	corner_radius = 9,
-	border_width = 2,
 })
