e85687d35df68f6b73b8f02238cdb9cf6f69dbf4
Conner <contact@conner-olsen.dev>
2024-12-05 00:24:12 -0500

    add latex snippets

---
+++ .config/nvim/lua/plugins/extras/latex-snippets.lua
@@ -0,0 +1,11 @@
+return {
+  "iurimateus/luasnip-latex-snippets.nvim",
+  brannch = "experimental-sympy",
+  -- vimtex isn't required if using treesitter
+  dependencies = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
+  config = function()
+    require("luasnip-latex-snippets").setup()
+    -- or setup({ use_treesitter = true })
+    require("luasnip").config.setup({ enable_autosnippets = true })
+  end,
+}
