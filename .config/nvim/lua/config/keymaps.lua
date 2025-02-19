c838bb78a26c42a397b6ed25fc0da3daf028711b
Conner <contact@conner-olsen.dev>
2025-02-19 00:04:18 -0500

    add inkscape nvim commands

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,3 +10,17 @@ wk.add({
 })
 
 vim.keymap.set("i", "<C-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", { desc = "Correct previous spelling error", expr = false })
+
+local inkscape = require("telescope_inkscape_figures")
+
+wk.add({
+  { "<leader>i", group = "Inkscape" },
+  { "<leader>is", inkscape.start_inkscape_manager, desc = "Start Inkscape Manager" },
+  { "<leader>ik", inkscape.stop_inkscape_manager, desc = "Kill Inkscape Manager" },
+  { "<leader>if", require("telescope").extensions.inkscape_figures.inkscape_figures, desc = "Find Figures" },
+  {
+    "<leader>ii",
+    "<Esc><cmd>exec 'r!inkscape-figures-manager new -f -d figures -l \"'.getline('.').'\"'<CR>kkkkkkdd:%s/marginfigure/figure/g<CR>:%s/{figures\\//{/g<CR>jf{a",
+    desc = "Insert Figure",
+  },
+})
